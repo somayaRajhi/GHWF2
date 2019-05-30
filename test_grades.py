2cef6c3b6816861f4d66b8ef9f2cc702796e4233
BAnderson6895 <BAnderson6895@gmail.com>
2019-05-30 11:52:26 -0400

    Add test for one grade

--- test_grades.py
+++ test_grades.py
@@ -10,3 +10,7 @@ def test_zero_grades():
 def test_single_grade():
     grades = [42]
     assert compute_hw_average(grades) == 42
+
+def test_one_grade():
+    grades = [20]
+    assert compute_hw_average(grades) == sum(grades) / len(grades)
